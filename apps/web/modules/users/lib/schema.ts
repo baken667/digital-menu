a3404aa9d6f357ca73fceedf23b38f6c4d5f2e6d
Baken <baken.wws@gmail.com>
2025-01-25 19:57:22 +0500

    create modules page.

---
+++ apps/web/modules/users/lib/schema.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+import { messages } from "@/lib/messages";
+import { UserRoles } from "@prisma/client";
+import { CreateUserSchema } from "@/schemas/users/create-user";
+
+export const UserLoginSchema = z.object({
+  email: z.string().email(messages.validation.email),
+  password: z.string().min(1, messages.validation.required),
+});
+
+export const UserCreateSchema = z.object({
+  name: z.string().min(1, messages.validation.required),
+  email: z
+    .string()
+    .email(messages.validation.email)
+    .min(1, messages.validation.required),
+  role: z.nativeEnum(UserRoles, {
+    message: messages.validation.email,
+  }),
+});
+
+export const EditUserSchema = CreateUserSchema;
